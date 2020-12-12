d85a1807b8196a43f33dc27e0bef7d30033f9e95
John Bradley <j.r.bradley014@gmail.com>
2020-12-12 15:07:29 -0500

    Add project files.

---
+++ ICanProgram/ICanProgram.cpp
@@ -0,0 +1,31 @@
+// ICanProgram.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+    std::string input;
+    cout << "Type\n";
+    //cin >> input;
+    //cout << "\n" << input;
+    enum value {Ace = 0, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King};
+    enum suit {Club= 0, Diamond, Heart, Spade};
+
+    for(int i = 0; i)
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ ICanProgram/deck.h
@@ -0,0 +1,8 @@
+#pragma once
+class deck
+{
+	int cards = 52;
+	void shuffle();
+
+};
+
